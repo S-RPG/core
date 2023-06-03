85424ca5b64e6d620e3fb9ba5162cf9146e72755
gSantiago22 <gustavog.gtsm@gmail.com>
2023-06-03 10:32:30 -0300

    feat/loja : adicionando a classe Loja

---
+++ src/classes/loja/loja.hpp
@@ -0,0 +1,25 @@
+#ifndef LOJA_H
+#define LOJA_H
+#include <vector>
+#include <map>
+#include <utility>
+#include <string>
+
+template<typename T>
+class Loja{
+public:
+    void pedido(T e);
+    _pedido = pedido(_itens);
+    void compra();
+    void entrega();
+
+private:
+    double _dinheiro;
+    std::vector<std::pair<double,std::string>>_pedido;      //O vetor de pedido vai salvar o nome do edido e seu preço
+    std::map<unsigned,std::pair<double,std::string>>_itens; //Cada item da lista vai ter um número referente a ele (unsigned), 
+                                                            //um preço (double) e um nome (string)
+    double _politica;                                       //alinhação política do personagem
+
+};
+
+#endif
