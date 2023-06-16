ea0aefeaede32079ce7b76f6bf86b74d5d4290e1
iamcalegari <iamcalegari20@gmail.com>
2023-06-16 18:48:36 -0300

    feat: inclui arquivos necessarios para a classe Decisao

---
+++ src/classes/decisao/decisao.hpp
@@ -0,0 +1,16 @@
+#ifndef DECISAO
+#define DECISAO
+
+#include <string>
+
+
+class Decisao {
+  private: 
+    std::string _texto;
+    char _alternativa;
+    double _impactoVitalidade;
+    double _impactoSanidade;
+
+};
+
+#endif
