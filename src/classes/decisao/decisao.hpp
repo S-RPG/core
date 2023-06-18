5a66b1004c2423d35481e42bc45e172df87c05f5
iamcalegari <iamcalegari20@gmail.com>
2023-06-18 18:51:05 -0300

    feat: implementa classe Decisao

--- src/classes/decisao/decisao.hpp
+++ src/classes/decisao/decisao.hpp
@@ -3,14 +3,32 @@
 
 #include <string>
 
+class Decisao
+{
+private:
+  char _alternativa;
+  double _impactoSanidade;
+  double _impactoVitalidade;
+  std::string _texto;
 
-class Decisao {
-  private: 
-    std::string _texto;
-    char _alternativa;
-    double _impactoVitalidade;
-    double _impactoSanidade;
+public:
+  Decisao(char alternativa, double impactoSanidade, double impactoVitalidade, std::string texto);
 
+  void setAlternativa(char alternativa);
+
+  void setImpactoSanidade(char impactoSanidade);
+
+  void setImpactoVitalidade(char impactoVitalidade);
+
+  void setTexto(std::string texto);
+
+  char getAlternativa();
+
+  double getImpactoSanidade();
+
+  double getImpactoVitalidade();
+
+  std::string getTexto();
 };
 
 #endif
