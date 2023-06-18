ed05136c094b3ac5d1f8c6dc1335e889cf9c6846
iamcalegari <iamcalegari20@gmail.com>
2023-06-18 19:07:06 -0300

    feat: define getters e setters em classe em Decisao

---
+++ src/classes/situacao/situacao.hpp
@@ -0,0 +1,26 @@
+#ifndef SITUCAO_H
+#define SITUACAO_H
+
+#include <decisao/decisao.hpp>
+
+#include <string>
+#include <map>
+
+class Situacao : Decisao
+{
+private:
+  std::string _contexto;
+  std::map<char, Decisao> _decisoes;
+  std::string _titulo;
+
+public:
+  Situacao(std::string contexto, std::map<char, Decisao> decisoes, std::string titulo);
+
+  void setContexto(std::string contexto);
+
+  void setDecisoes(char alternativa, Decisao decisao);
+
+  void setTitulo(std::string titulo);
+};
+
+#endif
