4cac18cc5bfca9499e4a9c1ecd80d55c4f42a3be
iamcalegari <iamcalegari20@gmail.com>
2023-06-30 19:23:24 -0300

    fix: altera grupo de dados para a Decisao

--- src/classes/decisao/decisao.hpp
+++ src/classes/decisao/decisao.hpp
@@ -1,34 +1,14 @@
-#ifndef DECISAO
-#define DECISAO
+#ifndef DECISAO_H
+#define DECISAO_H
 
 #include <string>
 
-class Decisao
+struct Decisao
 {
-private:
-  char _alternativa;
-  double _impactoSanidade;
-  double _impactoVitalidade;
-  std::string _texto;
-
-public:
-  Decisao(char alternativa, double impactoSanidade, double impactoVitalidade, std::string texto);
-
-  void setAlternativa(char alternativa);
-
-  void setImpactoSanidade(char impactoSanidade);
-
-  void setImpactoVitalidade(char impactoVitalidade);
-
-  void setTexto(std::string texto);
-
-  char getAlternativa();
-
-  double getImpactoSanidade();
-
-  double getImpactoVitalidade();
-
-  std::string getTexto();
+  char alternativa;
+  double impactoSanidade;
+  double impactoVitalidade;
+  std::string texto;
 };
 
 #endif
