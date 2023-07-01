bfe5bee8d09f016473025595bb87a65c02caf012
gSantiago22 <gustavog.gtsm@gmail.com>
2023-07-01 02:14:57 -0300

    feat: implementação da estrutura do item

---
+++ src/classes/item/item.hpp
@@ -0,0 +1,14 @@
+#ifndef ITME_H
+#define ITEM_H
+
+#include <iostream>
+#include <string>
+
+struct Item{
+    std::string nome;
+    double price;
+    float vitalidade;
+    float sanidade;
+};
+
+#endif
