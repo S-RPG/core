e0beb3f4d3fb49bc2aa84acd74d60f8cf4e966d3
gSantiago22 <gustavog.gtsm@gmail.com>
2023-07-02 11:15:04 -0300

    Implementações adicionais

--- src/classes/loja/loja.cpp
+++ src/classes/loja/loja.cpp
@@ -15,6 +15,10 @@ void Loja::compra(){
         total += (mapIn.begin()->second.first) * (it->second);
     }
 
+    //PRECISA SER ADICIONADO OPERAÇÃO QUE VERIFICA SE O DINHEIRO DO INVENTÁRIO É SUFICIENTE PARA O PEDIDO
+    //Caso contrário, ele tem que voltar para a opção de alterar o carrinho
+    //Pode ser que esssa função de remover seja feita nessa função mesmo
+
     std::cout<<"Total: @"<<total<<std::endl;
 
     _dinheiro -= total;           //como o dinheiro vai ser incluido? depois da classe inventário ser incluida isso vai ser feito
@@ -27,23 +31,36 @@ void Loja::entrega(){
 }
 
 
-void Loja::interface(){
+void Loja::interfaceLoja(){
     Loja::mostrarItens();
+    std::cout<<""<<std::endl;
+    std::cout<<"Adicionar item ao carriho (add)"<<"   "
+             <<"Remover item do carrinho (rmv)"<<"   "
+             <<"Mostrar carrinho (show)"<<"   "
+             <<"Sair (out)"<<std::endl;
 
     std::string opc;                            //opção
     unsigned cod=0;                             //código
     unsigned qnt=0;                             //quantidade
-    while(opc!="esc"){
+
+    compra:                                     //referência para o goto
+    while(opc!="out"){
         std::cin>>opc;
         if(opc=="add"){                           //adiciona
             std::cin>>cod>>qnt;
+            //verificar se a quantidade é válida
             Loja::pedido(cod,qnt);
         } else if(opc=="rmv"){                    //remove
             std::cin>>cod>>qnt;
             Loja::removePedido(cod,qnt);
         }else if(opc=="show"){
             Loja::mostrarCarrinho();
-        }else if(opc=="esc"){                     //sai da interface
+            std::cout<<""<<std::endl;
+            std::cout<<"Adicionar item ao carriho (add)"<<"   "
+             <<"Remover item do carrinho (rmv)"<<"   "
+             <<"Mostrar carrinho (show)"<<"   "
+             <<"Sair (out)"<<std::endl;
+        }else if(opc=="out"){                     //sai da interface
             break;
         }else {
             //erro de opção inválida
@@ -52,32 +69,50 @@ void Loja::interface(){
     std::cout<<"Carrinho:"<<std::endl;
     Loja::mostrarCarrinho();
 
-    char alt;                                   //alternativa
+    char alt;                                 //alternativa
     std::cout<<"Deseja finalizar a compra?"<<"  "<<"S/N"<<std::endl;
     std::cin>>alt;
 
-    if(alt=='S'){
+    if(alt=='S'){                             //vai para a seção de compra finalizar o pedido
         Loja::compra();
-    }else if(alt=='N'){
-        //voltar para a escolha
+    }else if(alt=='N'){                       //encontra uma situação antes da operação ser finalizada
+        std::cout<<"Alterar o carrinho (alt)"<<"   "<<"Cancelar a compra (dlt)"<<"  "<<"Finalizar (end)"<<std::endl;
+        std::cin>>opc;
+        if(opc=="alt"){                       //retorna ao while de edição do carrinho
+            goto compra;
+        } else if(opc=="dlt"){                //informa que o carrinho foi deletado para ele ser destruido direto no final do escopo da função
+            //destrutor do map
+        }else if(opc=="end"){                //vai para a seção de compra finalizar o pedido
+            Loja::compra();
+        }
     }else{
         //erro de opção inválida
     }
+
+    //destrutor do map
 }
 
 
 void Loja::pedido(unsigned cod, unsigned qnt) {
-    //std::map<unsigned, std::pair<double, std::string>> chaveItem = _itens; // Copia o mapa de itens
+    for (auto it = _itens.begin(); it != _itens.end(); ++it) {
+        const auto& mapIn = it->first;
+        const auto& situaItem = it->second;
+        auto innerIt = mapIn.find(cod);
 
-    auto it = _itens.find(cod); // Procura o item pelo código
-
-    if (it != _itens.end()) {
-        // Insere o item no carrinho junto com a quantidade
-        _carrinho.emplace(it, qnt);
-    } else {
-        // Implemente a ação para o caso de código de item inválido
-        // return; // ou outro tratamento de erro
+        if (innerIt != mapIn.end()) {
+            if ((innerIt->first == cod)&&(situaItem == "DISPONÍVEL")) {
+                const auto& par = innerIt->second;
+                _carrinho.emplace(innerIt, qnt);
+                std::cout << qnt << " " << par.second << " adicionados com sucesso!" << std::endl;
+                return; // Saia da função após a inserção no carrinho bem-sucedida
+            } else if ((innerIt->first == cod)&&(situaItem == "INDISPONÍVEL")){
+                std::cout << "Item indisponível" << std::endl;
+                return; // Saia da função, pois o item está indisponível
+            }
+        }
     }
+
+    std::cout << "Código inválido" << std::endl;
 }
 
 
@@ -95,10 +130,13 @@ void Loja::mostrarCarrinho(){
 
 
 void Loja::mostrarItens(){
-    auto it = _itens.begin();
+    auto it = _itens.begin();                       //copia o map de itens 
+    const auto& mapIn = _itens.begin()->first;      //separa a primeira parte do map de itens, já que é outro map
+    const auto& par = mapIn.begin()->second;        //separa o pair do map separado
 
     for( ; it!=_itens.end(); it++){
-        std::cout<<(it->first)<<" - "<<(it->second.second)<<" - @"<<(it->second.first)<<std::endl;
+        std::cout<<(mapIn.begin()->first)<<" - "<<(par.second)
+        <<" - @"<<(par.first)<<"  "<<"("<<(it->second)<<")"<<std::endl;
     }
 }
 
@@ -109,9 +147,8 @@ void Loja::removePedido(unsigned cod, unsigned qnt){
     }
 
     auto it = _carrinho.begin();
-
+    const auto& mapIn = it->first;
     for( ; it!=_carrinho.end(); it++){
-        const auto& mapIn = it->first;
         if(((mapIn.begin()->first)==cod)){
             break;
         }
@@ -120,9 +157,13 @@ void Loja::removePedido(unsigned cod, unsigned qnt){
     //implementar a opção de se nenhuma opção apareceu, retornar erro
 
     if((it->second)==qnt){
+        auto &name = mapIn.begin()->second;
+        std::cout<<"-"<<&name<<"totalmente removido!"<<std::endl;
         _carrinho.erase(it);
     } else if((it->second)>qnt){
+        auto &name = mapIn.begin()->second;
         (it->second)=(it->second)-qnt;
+        std::cout<<qnt<<"  "<<&name<<"removidos com sucesso!"<<std::endl;
     } else if((it->second)<qnt){
         //mensagem de erro, já que a quantidade a ser removida foi inválida
     }
--- src/classes/loja/loja.hpp
+++ src/classes/loja/loja.hpp
@@ -10,16 +10,16 @@
 //template<typename T>
 class Loja{
 private:
-    double _dinheiro;                                                                 //vai receber o valor do inventário
+    double _dinheiro;                                                                           //vai receber o valor do inventário
     std::unordered_map<std::map<unsigned,std::pair<double,std::string>>,unsigned>_carrinho;     //O vetor de pedido vai salvar o nome do pedido e seu preço
-    std::map<unsigned,std::pair<double,std::string>>_itens;                           //Cada item da lista vai ter um número referente a ele (unsigned), 
-                                                                                     //um preço (double) e um nome (string)
-    double _politica;                                                               //alinhação política do personagem recebida pela classe Personagem (remover??)
+    std::map<std::map<unsigned,std::pair<double,std::string>>,std::string>_itens;               //Cada item da lista vai ter um número referente a ele (unsigned), 
+                                                                                                //um preço (double) e um nome (string)
+    double _politica;                                                                           //alinhação política do personagem recebida pela classe Personagem (remover??)
 
 public:
     void compra();
     void entrega();
-    void interface();
+    void interfaceLoja();
     void pedido(unsigned cod, unsigned qnt);               //nenhuma dessas funções está bem definida ainda, os tipos e os parametros estão sendo pensados
     void mostrarCarrinho();
     void mostrarItens();
