60e06df1f87272a3a2fb17a9b39e115634d2d3ed
Wg-morais <washington94morais@gmail.com>
2023-07-05 21:36:03 -0300

    fix: incluindo comentarios para doxygen

--- src/classes/item/item.hpp
+++ src/classes/item/item.hpp
@@ -3,26 +3,30 @@
 
 #include <string>
 
+/**
+ * @brief Enumeração dos tipos de item.
+ */
 enum class TipoItem {
-    ALIMENTO,
-    ARMA,
-    ENTRETENIMENTO,
-    FERRAMENTA,
-    HIGIENE,
-    PRIMEIROS_SOCORROS
+    ALIMENTO,               /**< Item de alimento. */
+    ARMA,                   /**< Item de arma. */
+    ENTRETENIMENTO,         /**< Item de entretenimento. */
+    FERRAMENTA,             /**< Item de ferramenta. */
+    HIGIENE,                /**< Item de higiene. */
+    PRIMEIROS_SOCORROS      /**< Item de primeiros socorros. */
 };
 
+/**
+ * @brief Estrutura que representa um item.
+ */
 struct Item {
-
-    std::string nome;
-    TipoItem tipo;
-    bool consumivel;
-    float preco;
-    double vitalidade;
-    double sanidade;
-    unsigned quantidade;
-    unsigned id;
-    
+    std::string nome;       ///< Nome do item.
+    TipoItem tipo;          ///< Tipo do item.
+    bool consumivel;        ///< Indica se o item é consumível.
+    float preco;            ///< Preço do item.
+    double vitalidade;      ///< Valor de vitalidade do item.
+    double sanidade;        ///< Valor de sanidade do item.
+    unsigned quantidade;    ///< Quantidade do item disponível.
+    unsigned id;            ///< ID único do item.
 };
 
 #endif
