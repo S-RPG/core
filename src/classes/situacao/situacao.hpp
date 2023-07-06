a05dd915bd5ccfb33c0fb879ca74d93ccbf8b112
Wg-morais <washington94morais@gmail.com>
2023-07-06 08:26:03 -0300

    fix: alteração do estilo de comentarios

--- src/classes/situacao/situacao.hpp
+++ src/classes/situacao/situacao.hpp
@@ -1,18 +1,16 @@
-/**
- * @file situacao.h
- * @brief Definição da classe Situacao
- * Este arquivo contém a definição da classe Situacao, responsável pelo gerenciamento da situação do jogo.
- *
- * @version 1.0
- * @date 07/07/2023
- *
- * @authors
- * - Alan Mota Calegari
- * - Davi de Carvalho Clark
- * - Gustavo Santiago de Magalhães
- * - Ricardo Augusto Costa Brito Moraes
- * - Washington Aparecido de Jesus Morais
- */
+/// @file situacao.h
+/// @brief Definição da classe Situacao
+/// Este arquivo contém a definição da classe Situacao, responsável pelo gerenciamento da situação do jogo.
+///
+/// @version 1.0
+/// @date 07/07/2023
+///
+/// @authors
+/// - Alan Mota Calegari
+/// - Davi de Carvalho Clark
+/// - Gustavo Santiago de Magalhães
+/// - Ricardo Augusto Costa Brito Moraes
+/// - Washington Aparecido de Jesus Morais
 
 #ifndef SITUACAO_H
 #define SITUACAO_H
@@ -22,9 +20,7 @@
 #include <string>
 #include <map>
 
-/**
- * @brief Classe que representa uma situação no jogo.
- */
+/// @brief Classe que representa uma situação no jogo.
 class Situacao : Decisao
 {
 private:
@@ -33,52 +29,39 @@ private:
   std::string _titulo;                    ///< O título da situação.
 
 public:
-  /**
-   * @brief Construtor da classe Situacao.
-   *
-   * @param contexto O contexto da situação.
-   * @param decisoes As decisões disponíveis na situação.
-   * @param titulo O título da situação.
-   */
-  Situacao(std::string contexto, std::map<char, Decisao> decisoes, std::string titulo);
+  
+  /// @brief Construtor da classe Situacao.
+  ///
+  /// @param contexto O contexto da situação.
+  /// @param decisoes As decisões disponíveis na situação.
+  /// @param titulo O título da situação.
+Situacao(std::string contexto, std::map<char, Decisao> decisoes, std::string titulo);
 
-  /**
-   * @brief Define o contexto da situação.
-   * @param contexto O contexto da situação.
-   */
-  void setContexto(std::string contexto);
+/// @brief Define o contexto da situação.
+/// @param contexto O contexto da situação.
+void setContexto(std::string contexto);
 
-  /**
-   * @brief Define uma decisão para a situação.
-   *
-   * @param alternativa A alternativa da decisão.
-   * @param decisao A decisão a ser definida.
-   */
-  void setDecisoes(char alternativa, Decisao decisao);
+/// @brief Define uma decisão para a situação.
+///
+/// @param alternativa A alternativa da decisão.
+/// @param decisao A decisão a ser definida.
+void setDecisoes(char alternativa, Decisao decisao);
 
-  /**
-   * @brief Define o título da situação.
-   * @param titulo O título da situação.
-   */
-  void setTitulo(std::string titulo);
+/// @brief Define o título da situação.
+/// @param titulo O título da situação.
+void setTitulo(std::string titulo);
 
-  /**
-   * @brief Obtém o contexto da situação.
-   * @return O contexto da situação.
-   */
-  std::string getContexto();
+/// @brief Obtém o contexto da situação.
+/// @return O contexto da situação.
+std::string getContexto();
 
-  /**
-   * @brief Obtém as decisões disponíveis na situação.
-   * @return Um mapa com as decisões disponíveis, onde a chave é a alternativa da decisão e o valor é a própria decisão.
-   */
-  std::map<char, Decisao> getDecisoes();
+/// @brief Obtém as decisões disponíveis na situação.
+/// @return Um mapa com as decisões disponíveis, onde a chave é a alternativa da decisão e o valor é a própria decisão.
+std::map<char, Decisao> getDecisoes();
 
-  /**
-   * @brief Obtém o título da situação.
-   * @return O título da situação.
-   */
-  std::string getTitulo();
+/// @brief Obtém o título da situação.
+/// @return O título da situação.
+std::string getTitulo();
 };
 
 #endif
