cbfa9caa66aa9d16a4c98fef1613c0f7fb1e31c4
ricardoacbm <ricardomoraes460@gmail.com>
2023-07-06 15:12:54 -0300

    (remodelando as funções)

--- src/classes/inventario/inventario.cpp
+++ src/classes/inventario/inventario.cpp
@@ -3,47 +3,85 @@
 #include <vector>
 #include <utility>
 
+   
+
+    double getDinheiro(){
+            return dinheiro;
+      }
+
+
+    double _setDinheiro(){
+            this->dinheiro;
+       }
+
 
                          
   for(auto it:inventario){
     
-   string inventario::exibir(string inventario){ 
-       
-        for(i=0; i < inventario.size();i++){
-            std::cout<< it.first.first << it.first.second << it.second.first << it.second.second <<endl;
 
-             }    
-   }
+    void inventario::armazenar(map item) { // armazena itens comprados ou ganhos em eventos aleatórios 
+
+        
+    }       
+
+
+
+
+
+
+    
+       
+
+
+
+    void inventario::exibir(char I) { // exibe itens do inventário 
+      
+        if(char == "I"){
+            
+             for (auto it = inventario.begin(); it != inventario.end(); it++) {
+                    
+                    std::cout << it->first<<"(ID)"<< it->second.nome <<"(NOME)" << it->second.quantidade << "(QUANTIDADE)" << it->second.vitalidade <<"(VITALIDADE)" << it->second.sanidade << "(SANIDADE)" << std::endl;
+    
+            }
+        }
+    }
+
+    void inventario::void remover(map item) { // remove itens do inventário 
     
 
+        if(string == "rmv") {
+           
+           std::cin >> int ID;
+
+                for(auto it = map.begin(); it != map.end(); it++) {
+                    
+                    if(int ID == it->first) {
+
+                       std::cout << "Escreva a quantidade a remover"    
+                       
+                       std::cin >> int QTD;
 
 
 
 
-    void inventario::remover (string item){
-     
-     if(*quantidade <= quantidade && *quantidade >= 0){
+            }
+
+        }  
+
+    }
+
+
+
+
+
+    void inventario::consumir(map item) { // consome uma quantidade x de um item e ganha/perde atributos
+
+         if(quantidade >= *quantidade && *quantidade >= 0){
+                    quantidade = quantidade - *quantidade;
             
-            
-         for(i=0, i < inventario.size(); i++){
-                if(string item == it.second.first){
-                    delete pair[i];
-                }
-            }   quantidade = quantidade - *quantidade;
-        }
-    }  
-
-
-
-
-    void inventario::consumir(string){
-        
-    if(quantidade >= *quantidade && *quantidade >= 0){
-                quantidade = quantidade - *quantidade;
-            
-          for(i=0, i < par.size(); i++){
-                if(string == it.first.first){
-                    it.first.second = it.first.second - 1;
+                for(i=0, i < par.size(); i++){
+                    if(string == it.first.first){
+                        it.first.second = it.first.second - 1;
                 }
             }       
                     quantidade = quantidade - *quantidade;
@@ -54,19 +92,12 @@
         }
     }
   
-  }
+  
 
      
 
 
-        //IDEAL INICIAL
-        //void consumir (string){
-        // if(string ação == "usar" || "consumir" || "Usar" || "Consumir"){
-        //    std::cin >> new Item;
-        //    std::cin >> *quantidade.Item;
-
-
 
 return 0;
-    } 
+     
     
--- src/classes/inventario/inventario.hpp
+++ src/classes/inventario/inventario.hpp
@@ -10,56 +10,29 @@ class inventario {
    
     public:
         
-        double getMoney();
-        void exibir(char);
+        double getDinheiro();
+        
+        void exibir(char I); // exibe itens do inventário
+        
+        void armazenar(map item); // armazena itens comprados ou ganhos em eventos aleatórios
+
+        void remover(map item); // remove itens do inventário 
+
+        void consumir(map item); // consome uma quantidade x de um item e ganha/perde atributos
+
+
+            
 
     private:
         
         map <unsigned,Item> inventario; // <id,(nome, quantidade, sanidade e vitalidade)>  mapa de <id do item, variavel do tipo item>
         
-        double _money;
-        double _setMoney();
+        double _dinheiro;
         
-
-   
-
-inventario->first()
-inventario->second().nome
-inventario->second().vitalidade       
-
-    
-    
-       
-
-       
-        
-
-        
+        double _setDinheiro();
         
         
 }
-
-      double getMoney(){
-            return money;
-      }
-
-      double _setMoney(){
-            this->money;
-       }
-
-      
-
-    void armazenar(map item); // armazena itens comprados ou ganhos em eventos aleatórios
-            
-
-    void exibir(char I); // exibe itens do inventário
-       
-        cout << inventario->first() "(ID)" << inventario->second().nome "(NOME)" << inventario->second().quantidade "(QTD)" << inventario->second().sanidade"(SAN)" << inventario->second().vitalidade"(VIT)" << endl;
-
-    void remover(map item); // remove itens do inventário 
-
-
-    void consumir(map item); // consome uma quantidade x de um item e ganha/perde atributos
 
 
 
