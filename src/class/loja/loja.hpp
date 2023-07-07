10270390829561a4966f8250fffcf0e9c78dec41
Wg-morais <washington94morais@gmail.com>
2023-07-07 04:21:11 -0300

    fix: alteraçao de docstrings

--- src/class/loja/loja.hpp
+++ src/class/loja/loja.hpp
@@ -26,68 +26,83 @@
 #include <exception>
 
 
-//tratamento de exceções da encontrados na Loja
+/// @brief Classe de exceção para opção inválida.
 class InvalidOptionException : public std::exception {
 public:
+
+  /// @brief Retorna uma mensagem de erro descrevendo a exceção.
   const char *what() const noexcept override;
 };
 
-/// @brief Classe de exceção para código inválido
+/// @brief Classe de exceção para código inválido.
 class InvalidCodException : public std::exception {
 public:
 
-  /// @brief Construtor da classe InvalidCodException
-  /// @param message Mensagem de erro da exceção
+  /// @brief Construtor da classe InvalidCodException.
+  /// @param message Mensagem de erro da exceção.
   InvalidCodException(const std::string &message);
+
+  /// @brief Retorna uma mensagem de erro descrevendo a exceção.
   const char *what() const noexcept override;
   
 private:
-  std::string _message; ///< Mensagem de erro da exceção
+  std::string _message; ///< Mensagem de erro da exceção.
 };
 
-/// @brief Classe de exceção para quantidade inválida
+/// @brief Classe de exceção para quantidade inválida.
 class InvalidQntException : public std::exception {
 public:
 
-  /// @brief Construtor da classe InvalidQntException
-  /// @param message Mensagem de erro da exceção
+  /// @brief Construtor da classe InvalidQntException.
+  /// @param message Mensagem de erro da exceção.
   InvalidQntException(const std::string &message);
+
+  /// @brief Retorna uma mensagem de erro descrevendo a exceção.
   const char *what() const noexcept override;
   
 private:
-  std::string _message; ///< Mensagem de erro da exceção
+  std::string _message; ///< Mensagem de erro da exceção.
 };
 
-/// @brief Classe de exceção para carrinho inválido
+/// @brief Classe de exceção para carrinho inválido.
 class InvalidCarException : public std::exception {
 public:
 
-  /// @brief Construtor da classe InvalidCarException
-  /// @param message Mensagem de erro da exceção
+  /// @brief Construtor da classe InvalidCarException.
+  /// @param message Mensagem de erro da exceção.
   InvalidCarException(const std::string &message);
+
+  /// @brief Retorna uma mensagem de erro descrevendo a exceção.
   const char *what() const noexcept override;
   
 private:
-  std::string _message; ///< Mensagem de erro da exceção
+  std::string _message; ///< Mensagem de erro da exceção.
 };
 
-/// @brief Classe de exceção para valor monetário inválido
+/// @brief Classe de exceção para valor monetário inválido.
 class InvalidMoneyException : public std::exception {
 public:
+
+  /// @brief Retorna uma mensagem de erro descrevendo a exceção.
   const char *what() const noexcept override;
 };
 
-
-
+/// @brief Classe que representa uma loja.
 class Loja{
 private:
-    double _dinheiro;                                                                           //vai receber o valor do inventário
+
+    double _dinheiro;  ///< Armazena o valor do dinheiro disponível na loja.      
+    //vai receber o valor do inventário
+    
     //std::unordered_map<std::map<unsigned,std::pair<double,std::string>>,unsigned>_carrinho; //(remover) //O vetor de pedido vai salvar o nome do pedido e seu preço
-    std::map<unsigned,Item>_carrinho;                     //map com o código do produto e o item
+    
+    std::map<unsigned,Item>_carrinho; ///< Mapa que armazena os itens adicionados ao carrinho. Chave: código do produto. Valor: item.
+    
     //Inventario _inventario;         //Esperando a classe Inventário
     //std::map<std::map</*Item*/unsigned,std::pair<double,std::string>>,std::string>_itens;    //(remover)           //Cada item da lista vai ter um número referente a ele (unsigned), 
                                                                                                 //um preço (double) e um nome (string)
-    std::map<unsigned,std::pair<Item,bool>>_itens;       //map com o código do produto e um par do item e sua disponibilidade
+
+    std::map<unsigned,std::pair<Item,bool>>_itens; ///< Mapa que armazena os itens disponíveis na loja. Chave: código do produto. Valor: par (item, disponibilidade).
 
 public:
 
@@ -99,14 +114,34 @@ public:
     
     /// @brief Realiza a entrega dos itens comprados.
     void entrega();
+
+    /// @brief Obtém o carrinho de compras.
+    /// @return Mapa contendo o carrinho de compras.
     std::map<unsigned,Item> getCarrinho() const;
+
+    /// @brief Obtém a lista de itens disponíveis na loja.
+    /// @return Mapa contendo a lista de itens disponíveis na loja.
     std::map<unsigned,std::pair<Item,bool>> getItens() const;
+
+    /// @brief Mostra os itens no carrinho.
     void mostrarCarrinho();
 
     /// @brief Exibe os itens disponíveis na loja.
     void mostrarItens();
-    void pedido(unsigned cod, unsigned qnt);               //nenhuma dessas funções está bem definida ainda, os tipos e os parametros estão sendo pensados
+
+    /// @brief Realiza um pedido de compra.
+    ///
+    /// @param cod Código do item a ser pedido.
+    /// @param qnt Quantidade do item a ser pedido.
+    void pedido(unsigned cod, unsigned qnt);        //nenhuma dessas funções está bem definida ainda, os tipos e os parametros estão sendo pensados
+
+    /// @brief Remove um pedido do carrinho.
+    ///
+    /// @param cod Código do item a ser removido.
+    /// @param qnt Quantidade do item a ser removido.
     void removePedido(unsigned cod, unsigned qnt);
+
+    /// @brief Exibe as informações da loja.
     void showLoja();
 };
 
