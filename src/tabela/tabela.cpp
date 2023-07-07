55843228ad8fc74e5543597d02d25a0445c5eb51
gSantiago22 <gustavog.gtsm@gmail.com>
2023-07-07 01:56:54 -0300

    Configuração de tabela feita e funcionando

--- src/tabela/tabela.cpp
+++ src/tabela/tabela.cpp
@@ -49,39 +49,62 @@ struct Item {
     int quantidade;
 };
 
-void printTable(const std::map<unsigned, std::pair<Item, bool>>& items) {
-    std::cout << "-------------------------------------------------" << std::endl;
-    std::cout << std::left << std::setw(5) << "ID"
-              << " | " << std::setw(16) << "Nome"
-              << " | " << std::setw(8) << "Preço"
-              << " | " << std::setw(11) << "Quantidade" << std::endl;
-    std::cout << "--------------------------------------------------" << std::endl;
+void printTable(const std::map<unsigned, std::pair<Item, bool>>& itens) {
+    std::string itemStr;
+    std::size_t nomeTextLenMax = 0;
 
-    for (const auto& entry : items) {
-        const auto& id = entry.first;
-        const auto& item = entry.second.first;
-        const auto& disponivel = entry.second.second;
-
-        if (disponivel) {
-            std::cout << std::left << std::setw(5) << id
-                      << " | " << std::setw(16) << item.nome
-                      << " | @" << std::setw(6) << std::fixed << std::setprecision(2) << item.preco
-                      << " | " << std::setw(11) << item.quantidade << std::endl;
+    for (auto it=itens.begin(); it!=itens.end(); it++){
+        std::size_t nomeTextLen = it->second.first.nome.size();
+        if (nomeTextLenMax < nomeTextLen){
+            nomeTextLenMax = nomeTextLen;
         }
     }
 
-    std::cout << "--------------------------------------------------" << std::endl;
+    std::string cabeca = "Nome";
+    std::size_t cabecaTextLen = cabeca.size()/3;
+    std::size_t tab = nomeTextLenMax + cabecaTextLen;
+    
+     std::string linhaHoriz = "";
+
+    std::size_t totalSize = 5 + 6 + tab + 10 + 11;
+    linhaHoriz.append(totalSize, '-');
+
+    std::cout << linhaHoriz << std::endl;
+    std::cout << std::left << std::setw(5) << "ID"
+          << " | " << std::setw(tab) << "Nome"
+          << " | " << std::setw(8) << "Preço"
+          << " | " << std::setw(11) << "Quantidade" << std::endl;
+    std::cout << linhaHoriz << std::endl;
+
+    for (auto it=itens.begin(); it!=itens.end(); it++) {
+        const auto& id = it->first;
+        const auto& item = it->second.first;
+        const auto& disponivel = it->second.second;
+
+        if (disponivel) {
+            std::size_t nomeTextLen = nomeTextLenMax;
+            std::cout << std::left << std::setw(5) << id
+                      << " | " << std::setw(tab) << item.nome
+                      << " | @" << std::setw(6) << std::fixed << std::setprecision(2) << item.preco
+                      << " | " << std::setw(11) << item.quantidade << std::endl;
+
+        }
+    }
+
+    std::cout << linhaHoriz << std::endl;
 }
 
 int main() {
-    std::map<unsigned, std::pair<Item, bool>> items = {
+    std::map<unsigned, std::pair<Item, bool>> itens = {
         {1, {{ "Item 1", 10.0, 5 }, true}},
-        {2, {{ "Itenzão foda ", 15.0, 3 }, true}},
-        {3, {{ "Item 3", 20.0, 7 }, false}},
-        {4, {{ "Item 4", 12.0, 2 }, true}}
+        {2, {{ "Itenzao fodinha", 15.0, 3 }, false}},
+        {3, {{ "Item 3", 20.0, 7 }, true}},
+        {4, {{ "Item 4", 12.0, 2 }, true}},
+        {5,{{"PUTA QUE PARIU A", 23.0, 1},false}},
+        {6,{{"CARAIO A QUATRO DE ASA", 666.0, 1},false}}
     };
 
-    printTable(items);
+    printTable(itens);
 
     return 0;
 }
