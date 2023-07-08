8a1544c2b628d8b74647b341d7984737f3e792db
Wg-morais <130805659+Wg-morais@users.noreply.github.com>
2023-07-07 23:57:28 -0300

    feat-doc/database: incluindo comentatios em factory_situacao, factory_decisao e factory (#39)
    * fix: alteraçao de docstrings
    
    * fix: adicionando docstrings em factory_decisao, factory_situacao e factory
    
    * fix: editando comentarios em factory_situacao, factory_decisao e factory
    
    ---------
    
    Co-authored-by: iamcalegari <iamcalegari20@gmail.com>

--- src/class/loja/loja.hpp
+++ src/class/loja/loja.hpp
@@ -39,6 +39,10 @@ private:
   // um preço (float) e um nome (string)
   std::map<unsigned, std::pair<Item, bool>> _itens; // map com o código do produto e um par do item e sua disponibilidade
 
+<<<<<<< HEAD
+/// @brief Classe de exceção para opção inválida.
+class InvalidOptionException : public std::exception {
+=======
 public:
   /// @brief Realiza a compra dos itens no carrinho.
   /// @return True se a compra foi realizada com sucesso, False caso contrário.
@@ -64,52 +68,158 @@ public:
 // tratamento de exceções da encontrados na Loja
 class InvalidOptionException : public std::exception
 {
+>>>>>>> eea5279892b944e5fd32b9ad4b93ceb73b93b764
 public:
+
+  /// @brief Retorna uma mensagem de erro descrevendo a exceção.
   const char *what() const noexcept override;
 };
 
+<<<<<<< HEAD
+/// @brief Classe de exceção para código inválido.
+class InvalidCodException : public std::exception {
+public:
+
+  /// @brief Construtor da classe InvalidCodException.
+  /// @param message Mensagem de erro da exceção.
+=======
 /// @brief Classe de exceção para código inválido
 class InvalidCodException : public std::exception
 {
 public:
   /// @brief Construtor da classe InvalidCodException
   /// @param message Mensagem de erro da exceção
+>>>>>>> eea5279892b944e5fd32b9ad4b93ceb73b93b764
   InvalidCodException(const std::string &message);
+
+  /// @brief Retorna uma mensagem de erro descrevendo a exceção.
   const char *what() const noexcept override;
 
 private:
-  std::string _message; ///< Mensagem de erro da exceção
+  std::string _message; ///< Mensagem de erro da exceção.
 };
 
+<<<<<<< HEAD
+/// @brief Classe de exceção para quantidade inválida.
+class InvalidQntException : public std::exception {
+public:
+
+  /// @brief Construtor da classe InvalidQntException.
+  /// @param message Mensagem de erro da exceção.
+=======
 /// @brief Classe de exceção para quantidade inválida
 class InvalidQntException : public std::exception
 {
 public:
   /// @brief Construtor da classe InvalidQntException
   /// @param message Mensagem de erro da exceção
+>>>>>>> eea5279892b944e5fd32b9ad4b93ceb73b93b764
   InvalidQntException(const std::string &message);
+
+  /// @brief Retorna uma mensagem de erro descrevendo a exceção.
   const char *what() const noexcept override;
 
 private:
-  std::string _message; ///< Mensagem de erro da exceção
+  std::string _message; ///< Mensagem de erro da exceção.
 };
 
+<<<<<<< HEAD
+/// @brief Classe de exceção para carrinho inválido.
+class InvalidCarException : public std::exception {
+public:
+
+  /// @brief Construtor da classe InvalidCarException.
+  /// @param message Mensagem de erro da exceção.
+=======
 /// @brief Classe de exceção para carrinho inválido
 class InvalidCarException : public std::exception
 {
 public:
   /// @brief Construtor da classe InvalidCarException
   /// @param message Mensagem de erro da exceção
+>>>>>>> eea5279892b944e5fd32b9ad4b93ceb73b93b764
   InvalidCarException(const std::string &message);
+
+  /// @brief Retorna uma mensagem de erro descrevendo a exceção.
   const char *what() const noexcept override;
 
 private:
-  std::string _message; ///< Mensagem de erro da exceção
+  std::string _message; ///< Mensagem de erro da exceção.
 };
 
+<<<<<<< HEAD
+/// @brief Classe de exceção para valor monetário inválido.
+class InvalidMoneyException : public std::exception {
+=======
 /// @brief Classe de exceção para valor monetário inválido
 class InvalidMoneyException : public std::exception
 {
+>>>>>>> eea5279892b944e5fd32b9ad4b93ceb73b93b764
 public:
+
+  /// @brief Retorna uma mensagem de erro descrevendo a exceção.
   const char *what() const noexcept override;
 };
+<<<<<<< HEAD
+
+/// @brief Classe que representa uma loja.
+class Loja{
+private:
+
+    double _dinheiro;  ///< Armazena o valor do dinheiro disponível na loja.      
+    //vai receber o valor do inventário
+    
+    //std::unordered_map<std::map<unsigned,std::pair<double,std::string>>,unsigned>_carrinho; //(remover) //O vetor de pedido vai salvar o nome do pedido e seu preço
+    
+    std::map<unsigned,Item>_carrinho; ///< Mapa que armazena os itens adicionados ao carrinho. Chave: código do produto. Valor: item.
+    
+    //Inventario _inventario;         //Esperando a classe Inventário
+    //std::map<std::map</*Item*/unsigned,std::pair<double,std::string>>,std::string>_itens;    //(remover)           //Cada item da lista vai ter um número referente a ele (unsigned), 
+                                                                                                //um preço (double) e um nome (string)
+
+    std::map<unsigned,std::pair<Item,bool>>_itens; ///< Mapa que armazena os itens disponíveis na loja. Chave: código do produto. Valor: par (item, disponibilidade).
+
+public:
+
+    /// @brief Realiza a compra dos itens no carrinho.
+    /// @return True se a compra foi realizada com sucesso, False caso contrário.
+    bool compra();
+
+    //void definirDinheiro();         //Esperando a classe Inventário
+    
+    /// @brief Realiza a entrega dos itens comprados.
+    void entrega();
+
+    /// @brief Obtém o carrinho de compras.
+    /// @return Mapa contendo o carrinho de compras.
+    std::map<unsigned,Item> getCarrinho() const;
+
+    /// @brief Obtém a lista de itens disponíveis na loja.
+    /// @return Mapa contendo a lista de itens disponíveis na loja.
+    std::map<unsigned,std::pair<Item,bool>> getItens() const;
+
+    /// @brief Mostra os itens no carrinho.
+    void mostrarCarrinho();
+
+    /// @brief Exibe os itens disponíveis na loja.
+    void mostrarItens();
+
+    /// @brief Realiza um pedido de compra.
+    ///
+    /// @param cod Código do item a ser pedido.
+    /// @param qnt Quantidade do item a ser pedido.
+    void pedido(unsigned cod, unsigned qnt);        //nenhuma dessas funções está bem definida ainda, os tipos e os parametros estão sendo pensados
+
+    /// @brief Remove um pedido do carrinho.
+    ///
+    /// @param cod Código do item a ser removido.
+    /// @param qnt Quantidade do item a ser removido.
+    void removePedido(unsigned cod, unsigned qnt);
+
+    /// @brief Exibe as informações da loja.
+    void showLoja();
+};
+
+#endif
+=======
+>>>>>>> eea5279892b944e5fd32b9ad4b93ceb73b93b764
--- src/database/factory-decisao.hpp
+++ src/database/factory-decisao.hpp
@@ -1,5 +1,25 @@
+<<<<<<< HEAD
+/// @file factory_decisao.h
+/// @brief Definição da classe FactoryDecisao
+/// 
+/// Este arquivo contém a definição da classe FactoryDecisao, responsável por criar instâncias da classe Decisao.
+///
+/// @version 1.0
+/// @date 07/07/2023
+/// 
+/// @authors
+/// - Alan Mota Calegari
+/// - Davi de Carvalho Clark
+/// - Gustavo Santiago de Magalhães
+/// - Ricardo Augusto Costa Brito Moraes
+/// - Washington Aparecido de Jesus Morais
+///
+#ifndef FACTORY_SITUACAO_H
+#define FACTORY_SITUACAO_H
+=======
 #ifndef FACTORY_DECISAO_H
 #define FACTORY_DECISAO_H
+>>>>>>> eea5279892b944e5fd32b9ad4b93ceb73b93b764
 
 #include "factory.hpp"
 #include "factory-situacao.hpp"
@@ -10,19 +30,41 @@
 #include <vector>
 #include <memory>
 
+/// @brief Classe responsável por criar instâncias da classe Decisao.
 class FactoryDecisao : public Factory<Decisao>
 {
 public:
+<<<<<<< HEAD
+    /// @brief Cria uma instância da classe Decisao com base nos valores fornecidos.
+    /// @param instanceValues Os valores para criar a instância da classe Decisao.
+    /// @return Um ponteiro único para a instância da classe Decisao criada.
+    std::unique_ptr<Decisao> create(const std::vector<std::string> &instanceValues) override;
+
+    /// @brief Cria um mapa de decisões com base em um arquivo.
+    /// @param filename O nome do arquivo contendo as decisões.
+    /// @return Um mapa de decisões, onde a chave externa é o ID da situação e a chave interna é a alternativa da decisão.
+    std::map<unsigned, std::map<char, Decisao &>> Factory(std::string &filename);
+
+    /// @brief Preenche as decisões com base em uma instância de Decisao.
+    /// @param decisao A instância de Decisao a ser usada para preencher as decisões.
+    void populateDecisoes(const Decisao &decisao);
+=======
   std::shared_ptr<Decisao> create(const std::vector<std::string> &instanceValues) override;
 
   std::map<unsigned, std::map<char, Decisao>> Factory(std::string &filename);
   void populateDecisoes(const Decisao &decisao);
+>>>>>>> eea5279892b944e5fd32b9ad4b93ceb73b93b764
 
   friend class FactorySituacao;
 
 private:
+<<<<<<< HEAD
+    std::map<char, Decisao &> decisoes_dia;                    ///< Mapa de decisões por dia.
+    std::map<unsigned, std::map<char, Decisao &>> decisoes;    ///< Mapa de todas as decisões.
+=======
   std::map<char, Decisao &> decisoes_dia;
   static std::map<unsigned, std::map<char, Decisao>> decisoes;
+>>>>>>> eea5279892b944e5fd32b9ad4b93ceb73b93b764
 };
 
 #endif
