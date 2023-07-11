9e762593109e0addbd5206ae36f237ce97f7e676
Alan Calegari <64042095+iamcalegari@users.noreply.github.com>
2023-07-11 11:36:12 -0300

    wip (#54)

--- src/database/factory-decisao.cpp
+++ src/database/factory-decisao.cpp
@@ -1,97 +1,105 @@
-#include "factory.hpp"
-#include "../class/decisao/decisao.hpp"
 #include "factory-decisao.hpp"
 
-#include <fstream>
-#include <algorithm>
-#include <iostream>
-#include <map>
 #include <string>
 #include <vector>
-#include <memory>
+#include <map>
 
-/**
- * Crie um objeto de Decisão com base nos valores da instância.
- *
- * @param instanceValues - Um ponteiro para um vetor de strings contendo os valores de instância.
- * @return Um ponteiro para um objeto de Decisão criado.
- */
-std::shared_ptr<Decisao> FactoryDecisao::create(const std::vector<std::string> &instanceValues)
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+std::map<std::size_t, std::map<char, Decisao *>> FactoryDecisao::decisoes;
+
+void FactoryDecisao::create(const std::string &filename)
 {
-  auto it = instanceValues.begin();
-  unsigned id = std::stoul(*it++);
-  unsigned situacaoId = std::stoul(*it++);
-  std::string s = *it++;
-  char alternativa = s.at(0);
-  std::string texto = *it++;
-  float impactoSanidade = std::stof(*it++);
-  float impactoVitalidade = std::stof(*it++);
 
-  std::cout << "Decisao " << id << " - " << alternativa << ") " << texto << " criada com sucesso!"
-            << std::endl;
-
-  return std::make_shared<Decisao>(id, situacaoId, alternativa, texto, impactoSanidade, impactoVitalidade);
-}
-
-/**
- * Essa funcao popula o map 'decisoes_dia' na classe 'FactoryDecisao' com um objeto de Decisao.
- *
- * @param decisao O objeto de Decisao a ser inserido ou atribuido ao map.
- * @return None
- */
-void FactoryDecisao::populateDecisoes(const Decisao &decisao)
-{
-  this->decisoes_dia[decisao.alternativa] = decisao;
-}
-
-/**
- * FactoryDecisao::Factory - Crie um map de decisoes com base em um arquivo .csv (;)
- *
- * Essa funcao le um arquivo e cria um map de decisoes com base no
- * conteudo do arquivo. O map esta estruturado da seguinte forma:
- *   - As chaves do map externo sao inteiros positivos que representam
- *     IDs das situacoes.
- *   - Os valores do map externo sao maps internos.
- *   - As chaves dos maps internos sao caracteres que representam alternativas.
- *   - Os valores dos maps internos sao referencias aos objetos de Decisao.
- *
- * @param filename O nome do arquivo para ser lido
- * @return Um map de decisoes
- */
-std::map<unsigned, std::map<char, Decisao>> FactoryDecisao::Factory(std::string &filename)
-{
   std::ifstream file(filename);
 
-  // Conta o numero de linhas () do arquivo
-  unsigned lineCount = std::count(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>(), '\n');
-  this->removeLine(filename);
-
-  unsigned situacaoAtual = 1;
-  for (unsigned i = 0; i < lineCount; ++i)
+  if (!file)
   {
-    // Crie uma instância de Decisao com base no conteúdo do arquivo
-    std::vector<std::string> instanceValues = Factory::factory(filename);
-    auto decisao = this->create(instanceValues);
+    std::cerr << "File not found: " << filename << std::endl;
+  }
 
-    if (decisao == nullptr)
+  std::string line;
+  while (std::getline(file, line))
+  {
+    std::istringstream ss(line);
+    std::string field;
+
+    float impactoSanidade;
+    float impactoVitalidade;
+    std::string texto;
+    char alternativa;
+    unsigned id;
+    unsigned situacaoId;
+
+    if (std::getline(ss, field, ';'))
     {
+      id = std::stoul(field, nullptr, 10);
+    }
+    else
+    {
+      std::cerr << "Invalid id field" << std::endl;
       continue;
     }
 
-    // Verifica se o ID da decisao correspondente ao ID da situacao atual existe
-    if (decisao->id == situacaoAtual)
+    if (std::getline(ss, field, ';'))
     {
-      this->populateDecisoes(*decisao);
-      this->decisoes.insert_or_assign(situacaoAtual, this->decisoes_dia);
-      std::cout << "[!] Decisao atribuida a situacao " << decisao->situacaoId << " com sucesso!" << std::endl;
+      situacaoId = std::stoul(field, nullptr, 10);
+    }
+    else
+    {
+      std::cerr << "Invalid situacaoId field" << std::endl;
+      continue;
     }
 
-    situacaoAtual = decisao->id;
-    this->decisoes_dia.clear();
-    this->populateDecisoes(*decisao);
-    this->decisoes.insert_or_assign(situacaoAtual, this->decisoes_dia);
-    std::cout << "[+] Decisao criada para a situacao " << decisao->situacaoId << " com sucesso!" << std::endl;
-  }
+    if (std::getline(ss, field, ';'))
+    {
+      alternativa = field[0];
+    }
+    else
+    {
+      std::cerr << "Invalid alternativa field" << std::endl;
+      continue;
+    }
 
-  return decisoes;
+    if (std::getline(ss, field, ';'))
+    {
+      texto = field;
+    }
+    else
+    {
+      std::cerr << "Invalid texto field" << std::endl;
+      continue;
+    }
+
+    if (std::getline(ss, field, ';'))
+    {
+      auto _field = field == "-" ? "0.0" : field;
+
+      impactoSanidade = std::stof(_field, nullptr);
+    }
+    else
+    {
+      std::cerr << "Invalid impactoSanidade field" << std::endl;
+      continue;
+    }
+
+    if (std::getline(ss, field, ';'))
+    {
+      auto _field = field == "-" ? "0.0" : field;
+      impactoVitalidade = std::stof(_field, nullptr);
+    }
+    else
+    {
+      std::cerr << "Invalid impactoVitalidade field" << std::endl;
+      continue;
+    }
+
+    Decisao *decisao = new Decisao(id, situacaoId, alternativa, texto, impactoSanidade, impactoVitalidade);
+
+    this->decisoes[situacaoId][alternativa] = decisao;
+  }
 }
--- src/database/factory-situacao.cpp
+++ src/database/factory-situacao.cpp
@@ -1,99 +1,85 @@
-#include "factory.hpp"
-#include "../class/situacao/situacao.hpp"
 #include "factory-situacao.hpp"
+#include "factory-decisao.hpp"
 
-#include <fstream>
-#include <algorithm>
-#include <iostream>
-#include <map>
 #include <string>
 #include <vector>
-#include <memory>
+#include <map>
 
-/**
- * Cria um novo objeto Situacao com base nos valores de instância fornecidos.
- *
- * @param instanceValues Um vector de strings contendo os valores de instância.
- *
- * @return Um ponteiro unico para um objeto Situacao.
- *
- * @throws std::invalid_argument Se os valores da instancia forem invalidos.
- */
-std::shared_ptr<Situacao> FactorySituacao::create(const std::vector<std::string> &instanceValues)
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+std::map<std::size_t, std::vector<Situacao *>> FactorySituacao::situacoes;
+
+void FactorySituacao::create(const std::string &filename)
 {
-  std::vector<std::string>::const_iterator it = instanceValues.begin();
-  unsigned id = std::stoul(*it++, nullptr, 10);
-  std::string titulo = *it++;
-  std::string contexto = *it++;
-  unsigned decisoesId = std::stoul(*it++, nullptr, 10);
-  unsigned dia = std::stoul(*it++, nullptr, 10);
-  unsigned situacaoConectadaId = 0;
-  std::string _situacaoConectadaId = *it++;
-  if (!_situacaoConectadaId.empty())
-  {
-    situacaoConectadaId = std::stoul(_situacaoConectadaId, nullptr, 10);
-  }
 
-  std::cout << "Situacao " << id << " - " << titulo << " criada com sucesso!" << std::endl;
-  return std::make_shared<Situacao>(id, titulo, contexto, decisoesId, dia, situacaoConectadaId);
-};
-
-/**
- * Esta funcao adiciona um objeto 'situacao' ao vetor 'situacoes_dia'.
- *
- * @param situacao O objeto 'situacao' a ser adicionado.
- *
- * @return void
- *
- * @throws std::invalid_argument se o ponteiro 'this' for nulo.
- */
-void FactorySituacao::populateDia(const Situacao &situacao)
-{
-  if (this == nullptr)
-  {
-    throw std::invalid_argument("FactorySituacao instance is null");
-  }
-
-  this->situacoes_dia.push_back(situacao);
-}
-
-/**
- * FactorySituacao::Factory e uma funcao C++ que recebe um parametro de string `filename` e retorna um `std::map<unsigned, std::vector<Situacao>>`.
- *
- * @param filename uma string representando o nome do arquivo a ser processado
- *
- * @return um map que associa um inteiro positivo representando um dia com um vector de objetos Situacao
- *
- * @throws None
- */
-std::map<unsigned, std::vector<Situacao>> FactorySituacao::Factory(std::string &filename)
-{
   std::ifstream file(filename);
 
-  unsigned lineCount = std::count(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>(), '\n');
-  Factory::removeLine(filename);
-
-  unsigned diaAtual = 1;
-  for (unsigned i = 0; i < lineCount; ++i)
+  if (!file)
   {
+    std::cerr << "File not found: " << filename << std::endl;
+  }
 
-    std::vector<std::string> instanceValues = Factory::factory(filename);
-    auto situacao = this->create(instanceValues);
-    if (situacao->_dia == diaAtual)
+  std::string line;
+  while (std::getline(file, line))
+  {
+    std::istringstream ss(line);
+    std::string field;
+
+    unsigned id;
+    std::string titulo;
+    std::string contexto;
+    unsigned dia;
+    std::map<char, Decisao *> decisoes;
+    // unsigned situacaoConectadaId;
+
+    if (std::getline(ss, field, ';'))
     {
-      this->populateDia(*situacao);
-      this->situacoes_dias.insert_or_assign(diaAtual, this->situacoes_dia);
-      std::cout << "[!] Situacao " << situacao->_id << " - " << situacao->_titulo << " atribuida ao dia " << situacao->_dia << " com sucesso!" << std::endl;
+      id = std::stoul(field, nullptr, 10);
     }
     else
     {
-      diaAtual = situacao->_dia;
-      situacoes_dia.clear();
-      this->populateDia(*situacao);
-      this->situacoes_dias.insert_or_assign(diaAtual, this->situacoes_dia);
-      std::cout << "[+] Dia " << diaAtual << " criado e situacao " << situacao->_id << " - " << situacao->_titulo << " atribuida ao dia " << situacao->_dia << " com sucesso!" << std::endl;
+      std::cerr << "Invalid id field" << std::endl;
+      continue;
     }
-  }
 
-  return this->situacoes_dias;
-};
+    if (std::getline(ss, field, ';'))
+    {
+      titulo = field;
+    }
+    else
+    {
+      std::cerr << "Invalid titulo field" << std::endl;
+      continue;
+    }
+
+    if (std::getline(ss, field, ';'))
+    {
+      contexto = field;
+    }
+    else
+    {
+      std::cerr << "Invalid contexto field" << std::endl;
+      continue;
+    }
+
+    if (std::getline(ss, field, ';'))
+    {
+      dia = std::stoul(field, nullptr, 10);
+    }
+    else
+    {
+      std::cerr << "Invalid dia field" << std::endl;
+      continue;
+    }
+
+    decisoes = FactoryDecisao::decisoes[dia];
+
+    Situacao *situacao = new Situacao(id, titulo, contexto, decisoes, dia);
+
+    this->situacoes[dia].push_back(situacao);
+  }
+}
